507ebb2e07797ad0ab085e6eb1cb73312ebf450d
Fabricio Valadares <fabricio.valadares@gmail.com>
2020-09-17 18:23:50 -0300

    First commit
    BMI calculator - Project created at Mobile development course.
    
    This is just the screen. We will develop the algoritm together.

---
+++ app/src/main/java/br/com/pitagoras/aula1709/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.pitagoras.aula1709
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "FGV"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
